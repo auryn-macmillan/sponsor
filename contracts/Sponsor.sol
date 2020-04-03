793321b3b1c9dd9d0789fd08421b413089f49e18
Auryn Macmillan <aurynmacmillan@gmail.com>
2020-04-03 00:05:07 -0400

    ADD sponsorAdd function

--- contracts/Sponsor.sol
+++ contracts/Sponsor.sol
@@ -1,18 +1,24 @@
+
 pragma solidity ^0.6.4;
 
 import "./BrightID.sol";
 
 contract Sponsor {
-  BrightID public brightID;
-  bytes32 public context;
+ BrightID public brightID;
+ bytes32 public context;
 
-  constructor(BrightID _brightID, bytes32 _context) public {
-    brightID = _brightID;
-    context = _context;
-  }
+ constructor(BrightID _brightID, bytes32 _context) public {
+   brightID = _brightID;
+   context = _context;
+ }
 
-  // sponsor any address that sends a transaction to this contract.
-  function sponsor() public payable {
-    brightID.sponsor(context, bytes32(uint(msg.sender)));
-  }
+ // sponsor any address that sends a transaction to this contract.
+ function sponsor() public payable {
+   brightID.sponsor(context, bytes32(uint(msg.sender)));
+ }
+
+ // sponsor any address is provided by as an parameter.
+ function sponsor(address add) public {
+   brightID.sponsor(context, bytes32(uint(add)));
+ }
 }
